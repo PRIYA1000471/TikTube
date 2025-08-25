3637d8b6ab424a0970e34c73ead0423e3d862198
PRIYA1000471 <bushrapriya6@gmail.com>
2025-08-25 13:38:59 +0100

    changed manage.py file

--- manage.py
+++ manage.py
@@ -2,14 +2,11 @@
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+def main():
+    settings_module = 'config.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'config.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
